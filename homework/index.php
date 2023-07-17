87c7c9b8b76b3d7040cffa2c3c0152b94b187600
cndofx <19335833+cndofx@users.noreply.github.com>
2023-07-17 17:21:14 -0600

    homework

---
+++ homework/index.php
@@ -0,0 +1,34 @@
+<?php
+session_name('sesh');
+session_set_cookie_params(0, '/', '.sproing.us');
+session_start();
+if (!isset($_SESSION['loggedin'])) {
+   header('Location: bad.html');
+   exit;
+echo 'hi';
+echo $_SESSION['loggedin'];
+// If the user is not logged in redirect to the login page...
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="./secret.css">
+</head>
+<body>
+<div class="upper-title">
+<h1>secret art</h1>
+</div>
+<div class="text-box">
+<p>we hope you enjoy your stay</p>
+</div>
+<div class="lower-title">
+<h1>gallery</h1>
+</div>
+<div class="art">
+<h2>takemi zone</h2>
+<img src="action.jpeg"/>
+<img src="takemi.jpeg">
+</div>
+</body>
+</html>
