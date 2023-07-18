084b1045ed5ecd416c7940d61690103d44f51193
cndofx <19335833+cndofx@users.noreply.github.com>
2023-07-18 01:19:29 -0600

    run autoformatter

--- homework/index.php
+++ homework/index.php
@@ -4,46 +4,48 @@ session_name('sesh');
 session_set_cookie_params(0, '/', '.sproingus.com');
 session_start();
 if (!isset($_SESSION['loggedin'])) {
-   header('Location: bad.html');
-   exit;
-echo 'hi';
-echo $_SESSION['loggedin'];
-
-// If the user is not logged in redirect to the login page...
+    header('Location: bad.html');
+    exit;
+    echo 'hi';
+    echo $_SESSION['loggedin'];
+    // If the user is not logged in redirect to the login page...
 }
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
-<link rel="stylesheet" type="text/css" href="./secret.css">
+    <link rel="stylesheet" type="text/css" href="./secret.css">
 </head>
+
 <body>
-<div class="upper-title">
-<h1>secret art</h1>
-</div>
-<div class="text-box">
-<p><b>we hope you enjoy your stay</b></p>
-</div>
-<div class="lower-title">
-<h1>gallery</h1>
-</div>
-<div class="art">
-<h2>the zone</h2>
-</div>
-<form action="upload.php" method="post" enctype="multipart/form-data">
-	<input type="file" name="filetoupload" id="filetoupload">
-	<input type="submit" name="submit">
-</form>
-<?php
-        $query = "select * from images";
-        $result = $db->query($query);
- 
-        while ($data =mysqli_fetch_assoc($result)) {
+    <div class="upper-title">
+        <h1>secret art</h1>
+    </div>
+    <div class="text-box">
+        <p><b>we hope you enjoy your stay</b></p>
+    </div>
+    <div class="lower-title">
+        <h1>gallery</h1>
+    </div>
+    <div class="art">
+        <h2>the zone</h2>
+    </div>
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+        <input type="file" name="filetoupload" id="filetoupload">
+        <input type="submit" name="submit">
+    </form>
+    <?php
+    $query = "select * from images";
+    $result = $db->query($query);
+
+    while ($data = mysqli_fetch_assoc($result)) {
         ?>
-            <img src="/<?php echo $data['filename']; ?>">
- 
+        <img src="/<?php echo $data['filename']; ?>">
+
         <?php
-        }
-?>
+    }
+    ?>
 </body>
+
 </html>
--- main/mosssite/myform.php
+++ main/mosssite/myform.php
@@ -1,22 +1,22 @@
 <?php
 session_name('sesh');
-session_set_cookie_params(0,'/', 'sproingus.com');
+session_set_cookie_params(0, '/', 'sproingus.com');
 session_start();
-$password=$_POST['password'];
-$username=$_POST['username'];
+$password = $_POST['password'];
+$username = $_POST['username'];
 $dbhost_name = 'db5013773760.hosting-data.io';
 $database = 'dbs11528972';
 $dbuser_name = 'dbu1311756';
 $dbpassword = '7UfKsp5cJnWhKJ';
 $hashedpass = password_hash($password, PASSWORD_DEFAULT);
-$conn = mysqli_connect($dbhost_name, $dbuser_name,$dbpassword, $database);
+$conn = mysqli_connect($dbhost_name, $dbuser_name, $dbpassword, $database);
 $sql = "SELECT password FROM users WHERE username='$username'";
 $result = $conn->query($sql);
-$row = $result -> fetch_array(MYSQLI_NUM);
-if(password_verify($password,$row[0])){
-   session_regenerate_id(); 
-	$_SESSION['loggedin'] = 'TRUE';
-	header('Location: http://secret.sproingus.com/index.php');
+$row = $result->fetch_array(MYSQLI_NUM);
+if (password_verify($password, $row[0])) {
+    session_regenerate_id();
+    $_SESSION['loggedin'] = 'TRUE';
+    header('Location: http://secret.sproingus.com/index.php');
     exit;
 }
 ?>
